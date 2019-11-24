4b1fbf458ad339394a1f4e1bcb16f19fc25e8dee
oViCo1023 <55197303+oViCo1023@users.noreply.github.com>
2019-11-24 14:14:38 -0500

    changing coordinate relationship of cells in block

--- BlockFiles/SBlock.cc
+++ BlockFiles/SBlock.cc
@@ -1,10 +1,10 @@
 #include "SBlock.h"
 
 SBlock::SBlock(const int row, const int col, const int level): Block{level, 4}{
-    cells.emplace_back(row, col, BlockType::SBlock);
     cells.emplace_back(row, col + 1, BlockType::SBlock);
-    cells.emplace_back(row + 1, col - 1, BlockType::SBlock);
+    cells.emplace_back(row, col + 2, BlockType::SBlock);
     cells.emplace_back(row + 1, col, BlockType::SBlock);
+    cells.emplace_back(row + 1, col + 1, BlockType::SBlock);
 }
 
 std::vector<Cell>& SBlock::getCells(){
